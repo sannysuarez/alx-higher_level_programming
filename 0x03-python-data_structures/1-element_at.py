d169f2a1452f5cddc0b587ea7c6b1f466c5269c7
sanny suarez <68450026+sannysuarez@users.noreply.github.com>
2023-09-08 16:28:05 +0100

    Add files via upload
    fixed

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    if idx >= len(my_list):
+        return None
+    else:
+        index = my_list[3]
+        return index
