537393178a537bc0c01da378eb12c501689409a0
Sanni Suarez <sannysuarez4@gmail.com>
2023-09-09 08:52:09 -0700

    gerrit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]]
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix != [[]]:
+        for row in matrix:
+            for elem in row:
+                print("{:d}".format(elem), end=" "
+                      if elem != row[-1] else '\n')
+    else:
+        print()
