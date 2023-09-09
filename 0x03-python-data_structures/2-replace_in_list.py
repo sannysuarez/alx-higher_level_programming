bdb3afa3518ea69aea84bbe0c949253d475b131d
Sanni Suarez <sannysuarez4@gmail.com>
2023-09-09 07:03:03 -0700

    gerrir

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,10 @@
-#!/usr/bin/pytho3
+#!/usr/bin/python3
+
 def replace_in_list(my_list, idx, element):
+    n = len(my_list)
     if idx < 0:
         return my_list
-    elif idx >= len(my_list):
+    elif idx > n - 1:
         return my_list
     else:
         my_list[idx] = element
